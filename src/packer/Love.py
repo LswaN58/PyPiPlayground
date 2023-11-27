5be2040e77edb1ce46805af245883e10e8fc3505
Luke Swanson <lwswanson2@wisc.edu>
2023-11-27 15:24:34 -0600

    Trying to find import path that isn't being marked as bad by linter.

--- src/packer/Love.py
+++ src/packer/Love.py
@@ -1,6 +1,6 @@
 """Simple test module for importing from another file within a package
 """
-from src.packer.Dillon import Handoff
+from packer.Dillon import Handoff
 
 def Give():
     Handoff(left=True)
