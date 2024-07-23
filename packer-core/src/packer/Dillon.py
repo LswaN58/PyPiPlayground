f599401b4e254ecfc3a913ca35ef72c99397e874
Luke Swanson <lwswanson2@wisc.edu>
2024-07-22 23:02:56 -0500

    Add whatever latest changes were.

--- packer-core/src/packer/Dillon.py
+++ packer-core/src/packer/Dillon.py
@@ -1,7 +1,13 @@
 """Simple module whose function can be imported in another package.
 """
+from importlib.resources import files
+
 def Handoff(left:bool):
     if left:
         print("Dillon takes the handoff to the left.")
     else:
         print("Dillon takes the handoff to the right.")
+
+def Stats():
+    stats_text = files("packer").joinpath("DillonStats.txt").read_text()
+    print(f"Stats for AJ Dillon:\n{stats_text}")
